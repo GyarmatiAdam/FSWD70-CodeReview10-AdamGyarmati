45c896f8e1401c094a55e366da7747c3d7900801
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-07-12 15:58:36 +0200

    Connection
    DB connected with PDO connection

--- index.php
+++ index.php
@@ -1,3 +1,56 @@
 <?php
-    echo "Hello";
+    require_once('dbconnection.php');
 ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+        <a class="nav-item nav-link" href="#">BOOK</a>
+        <a class="nav-item nav-link" href="#">CD</a>
+        <a class="nav-item nav-link" href="#">DVD</a>
+        </div>
+    </div>
+    </nav>
+    <h1>The Big Library</h1>
+
+    <?php
+
+        try
+        {
+            $database = new Connection();
+            $db = $database->openConnection();
+            $sql = "SELECT * FROM library " ;
+            foreach ($db->query($sql) as $row) {
+            echo " ID: ".$row['library_id'] . "<br>";
+            echo " Title: ".$row['title'] . "<br>";
+            echo " ISBN: ".$row['isbn_code'] . "<br>";
+            echo " Type: ".$row['lib_type'] . "<br>";
+            }
+        }
+        catch (PDOException $e)
+        {
+            echo "There is some problem in connection: " . $e->getMessage();
+        }
+
+    ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
